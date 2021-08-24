d49d38a0e38144560f3b8b76972e339123694ce9
Himanshu Maurya <40685364+refoxdev@users.noreply.github.com>
2021-08-24 19:44:45 +0530

    ui header bug fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,99 +13,7 @@ import { getAllPosts } from '../lib/api';
 export const Index = (): JSX.Element => {
   return (
     <Layout>
-      <ul className=" flex justify-evenly  font-semibold p-4 ">
-        <li className="text-center ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 m-auto"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>Stories</span>
-        </li>
-        <li className="text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 m-auto"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-            />
-          </svg>
-          <span>Highlights</span>
-        </li>
-        <li className="text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 m-auto"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-            />
-          </svg>
-          <span>Photo</span>
-        </li>
-        <li className="text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 m-auto"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-            />
-          </svg>
-          <span>Video</span>
-        </li>
-        <li className="text-center ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 m-auto"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"
-            />
-          </svg>
-          <span className="m-auto">IGTV</span>
-        </li>
-      </ul>
+
       <h1 className="font-bold text-2xl mb-1 mt-5">Download Instagram Reels</h1>
       <p className="text-gray-500 ">
         Search and download Instagram Reels video with our Reels Downloader
@@ -134,7 +42,7 @@ export const Index = (): JSX.Element => {
               clipRule="evenodd"
             />
           </svg>
-        <span className="text-sm mr-1">Download</span>  
+          <span className="text-sm mr-1">Download</span>
         </button>
       </div>
       {/* 
